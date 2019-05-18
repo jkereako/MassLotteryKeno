d9132576684e68416ef883c50bf31b6229309439
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2019-05-18 18:43:31 -0400

    Incorporate Kringle

---
+++ Podfile
@@ -0,0 +1,19 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '11.0'
+
+target 'MassLotteryKeno' do
+  use_frameworks!
+
+  pod 'Kringle'
+
+  target 'MassLotteryKenoTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'MassLotteryKenoUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ Sources/MassLotteryEndpoint.swift
@@ -0,0 +1,25 @@
+//
+//  MassLotteryEndpoint.swift
+//  MassLotteryKeno
+//
+//  Created by Jeff Kereakoglow on 5/18/19.
+//  Copyright © 2019 Alexis Digital. All rights reserved.
+//
+
+import Kringle
+
+enum MassLotteryEndpoint {
+    case todaysGames
+}
+
+extension MassLotteryEndpoint: Endpoint {
+    var baseURL: URL { return URL(string: "http://www.masslottery.com/data/json")! }
+
+    var path: String {
+        switch self {
+        case .todaysGames:
+            return "/search/dailygames/todays/15.json"
+
+        }
+    }
+}
